39aab36f1b06a0fd69b8ab69a2edb5358ec6f99a
bzeromo <dudrb5260@naver.com>
2025-05-30 15:51:38 +0900

    Add query

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into users(userId, password, name, email) values ('azeromo', 'azero', '이영규', 'azero@bzero.com');
 insert into users(userId, password, name, email) values ('bzeromo', 'bzero', '박영규', 'bzero@bzero.com');
 insert into users(userId, password, name, email) values ('czeromo', 'czero', '김영규', 'czero@bzero.com');
+
+insert into employees
+select * from csvread('classpath:employees.csv', null, 'charset=UTF-8 fieldSeparator=,');
